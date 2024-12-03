4e6c0edee0ee8f73509eb61928f09e0ab25d9399
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2024-12-03 21:30:16 +0300

    add sound and almost finish the game

---
+++ README.md
@@ -0,0 +1,15 @@
+# <h1> WordRush Game
+![WordRush Logo Images](/img/WordRush%20logo.png)
+
+# <h2> About WordRush
+WordRush is a fast-paced and exciting word puzzle game designed to test your vocabulary and quick-thinking skills. In each level, you'll be presented with a random arrangement of letters, and your challenge is to form meaningful words before time runs out. With each completed level, the game gets progressively harder, offering more challenging letter combinations and stricter time limits. WordRush is perfect for players of all ages who love a fun and competitive brain exercise!
+[Getting Started](https://murtadha88.github.io/WordRush-Game/)
+
+# <h2> Technologies
+* **HTML**
+* **CSS**
+* **Javascript**
+* **Visual Studio**
+
+# <h2> Furture Plan
+In the future, I plan to expand the game by introducing more levels to challenge players as they progress. Each level will bring increased difficulty, such as time constraints or more complex letter arrangements. Additionally, I aim to add more categories, including themes like countries, and professions, to diversify the gameplay and make it more engaging. This expansion will allow players to choose categories that match their interests or challenge their knowledge in different areas.
--- js/app.js
+++ js/app.js
@@ -14,11 +14,8 @@ const animals1 = [
     { 1: "B", 2: "E", 3: "E" },
     { 1: "C", 2: "U", 3: "B" },
     { 1: "E", 2: "E", 3: "L" },
-    { 1: "K", 2: "I", 3: "T" },
     { 1: "Y", 2: "A", 3: "K" },
-    { 1: "D", 2: "O", 3: "E" },
     { 1: "H", 2: "O", 3: "G" },
-    { 1: "P", 2: "U", 3: "P" },
 ];
 
 const animals2 = [
@@ -42,6 +39,8 @@ const animals2 = [
     { 1: "P", 2: "I", 3: "K", 4: "A" },
     { 1: "T", 2: "O", 3: "A", 4: "D" },
     { 1: "M", 2: "I", 3: "N", 4: "K" },
+    { 1: "D", 2: "U", 3: "C", 4: "K" },
+    { 1: "T", 2: "U", 3: "N", 4: "A" },
 ];
 
 const animals3 = [
@@ -56,15 +55,15 @@ const animals3 = [
     { 1: "H", 2: "Y", 3: "E", 4: "N", 5: "A" },
     { 1: "P", 2: "I", 3: "G", 4: "E", 5: "O" },
     { 1: "P", 2: "A", 3: "N", 4: "D", 5: "A" },
-    { 1: "C", 2: "R", 3: "A", 4: "N", 5: "E" },
-    { 1: "T", 2: "U", 3: "N", 4: "A", 5: "S" },
     { 1: "W", 2: "H", 3: "A", 4: "L", 5: "E" },
     { 1: "O", 2: "T", 3: "T", 4: "E", 5: "R" },
     { 1: "S", 2: "L", 3: "O", 4: "T", 5: "H" },
     { 1: "S", 2: "N", 3: "A", 4: "I", 5: "L" },
     { 1: "R", 2: "A", 3: "V", 4: "E", 5: "N" },
-    { 1: "D", 2: "U", 3: "C", 4: "K", 5: "S" },
-    { 1: "G", 2: "O", 3: "A", 4: "T", 5: "S" },
+    { 1: "H", 2: "O", 3: "R", 4: "S", 5: "E" },
+    { 1: "E", 2: "L", 3: "A", 4: "N", 5: "D" },
+    { 1: "N", 2: "A", 3: "N", 4: "D", 5: "U" },
+    { 1: "S", 2: "K", 3: "U", 4: "N", 5: "K" },
 ];
 
 const names1 = [
@@ -233,6 +232,14 @@ let selectedCategory;
 let selectedData;
 let correctWord = "";
 
+const correctSound = new Audio('/audio/correct.mp3');
+const wrongSound = new Audio('/audio/wrong.mp3');
+const winSound = new Audio('/audio/win.mp3');
+const crowdSound = new Audio('/audio/crowd.mp3');
+const loseSound = new Audio('/audio/lose.mp3');
+const timesUpSound = new Audio('/audio/timesUp.mp3');
+
+
 const place = document.querySelector(".letters");
 const submit = document.querySelector(".submit-button");
 const userWord = document.querySelector(".word-input");
@@ -264,9 +271,11 @@ function init() {
 function render() {
     letters = [];
     wordcount = 0;
-    userWord.value = null;
+    userWord.value = "";
     userWord.classList.remove("wrong-input");
-    count.textContent = "0/5";
+    timerDisplay.classList.remove("low-time");
+    timesUpSound.pause();
+    count.textContent = `${wordcount}/5`;
     init();
     randomWord();
     clearInterval(timerInterval);
@@ -283,10 +292,19 @@ startTimer();
 function updateTimer() {
     if (timeLeft <= 0) {
         timerDisplay.textContent = "0";
-        alert("Time's up! You didn't complete the level. Don't give up, try again!");
-        render();
+        loseSound.play();
+        timesUpSound.pause();
+
+        setTimeout(function() {
+            alert("Time's up! You didn't complete the level. Don't give up, try again!");
+            render();
+        }, 700);
     }
     else {
+        if (timeLeft <= 10) {
+            timesUpSound.play();
+            timerDisplay.classList.add("low-time")
+        }
         timerDisplay.textContent = timeLeft;
         timeLeft--;
     }
@@ -308,8 +326,13 @@ function randomWord() {
     const randomIndex = Math.floor(Math.random() * selectedData.length);
     const selectedWord = selectedData[randomIndex];
 
+    
     for (let key in selectedWord) {
         letters.push(selectedWord[key]);
+    }
+    console.log(letters);
+    for(let i=0; i<letters.length; i++) {
+        correctWord = correctWord+letters[i];
     }
 
     shuffledLetters = [...letters];
@@ -317,10 +340,7 @@ function randomWord() {
 
     for (let i = 0; i < 1000; i++) {
         let stringShuffledLetters = "";
-        
-        for(let j=0; j<letters.length; j++) {
-            correctWord = correctWord+letters[j];
-        }
+
         for(let k=0; k<letters.length; k++) {
             stringShuffledLetters = stringShuffledLetters+shuffledLetters[k];
         }
@@ -348,19 +368,27 @@ function game() {
         wordcount++;
         count.textContent = `${wordcount}/5`;
         userWord.value = "";
+        correctSound.play();
         init();
         randomWord();
         if (wordcount === 5) {
             if(level === 3) {
-                alert("Congratulations! You completed this Category, Try other Category");
-                window.location.href = "../categoriesPage.html";
+                crowdSound.play();
+
+                setTimeout(function() {
+                    alert("Congratulations! You completed this Category, try anther category.");
+                    window.location.href = "../categoriesPage.html";
+                }, 500);
             }
             else {
-                alert("Congratulations! You completed this level.");
+                winSound.play();
                 level++;
-                spanLevel.textContent = `Level ${level}`;
-                render();
-                getCategory();
+                setTimeout(function() {
+                    alert("Congratulations! You completed this level.");
+                    spanLevel.textContent = `Level ${level}`;
+                    render();
+                    getCategory();
+                }, 500);   
             }   
         }
         userWord.classList.remove("wrong-input");
@@ -368,6 +396,7 @@ function game() {
     else {
         userWord.classList.add("wrong-input");
         userWord.value = "";
+        wrongSound.play();
     }
 }
 
